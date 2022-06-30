329b9276dcf54f09051a96045181e33aba5348a8
Sibal M <sibalmarcl@gmail.com>
2022-06-30 17:59:15 +0800

    tried MVC post add and failed

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,8 +11,39 @@ const controller = {
     },
 
     getIndex: function (req,res) {
-        res.render('card_post.hbs');
-    }
+        db.findMany(card_post, {}, {_id: 0, name: 1, refno: 1, amount: 1}, (result) =>{
+            console.log(result);
+            res.render('index', {cardPosts:result});
+        })
+    },
+
+    addPost: function (req,res) {
+        res.render('addPost');
+    },
+
+    getProfile: function (req,res) {
+        
+    },
+
+    userLogout: function (req,res) {
+
+    },
+
+    submitPost: function (req,res) {
+        let data = {
+            title: req.query.title,
+            content: req.query.content,
+            upvotes: req.query.upvotes,
+            author: req.query.author,
+            image: req.query.image
+        }
+
+        db.insertOne(card_post, data, (result) => {
+            res.render("partials\\card_post", data, function(error, html) {
+                res.send(html);
+            });
+        });
+    },
 }
 
 module.exports = controller;
--- public/JS/index.js
+++ public/JS/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
+    $('#submit').click(function () {
+        var titleInp = document.forms['addPost_form']['title'].value;
+        var contentInp = document.forms['addPost_form']['content'].value;
+        var upvotesInp = document.forms['addPost_forms']['upvotes'].value;
+        var authorInp = document.forms['addPost_forms']['author'].value;
+        var imgInp = document.forms['addPost_forms']['image'].value;
+
+        let post = {
+            title: titleInp,
+            content: contentInp,
+            upvotes: upvotesInp,
+            author: authorInp,
+            image: imgInp
+        }
+
+        $.get('/submit-post', post, function(data, status){
+            $('#cardPosts').append(data);
+        })
+    });
 })
